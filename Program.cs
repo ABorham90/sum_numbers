a9577854af783683d5f0a6038b708693ade1a6c4
Ahmad Borham <ahmad.borham.dev@gmail.com>
2024-09-12 22:49:35 +0300

    multiply function added

--- Program.cs
+++ Program.cs
@@ -6,10 +6,16 @@
         {
             return num1 + num2;
         }
+        public static int multiply(int num1,int num2)
+        {
+            return num1 * num2;
+        }
         static void Main(string[] args)
         {
             int result=sum(10, 20);
+            int Result=multiply(10, 20);
             Console.WriteLine(result.ToString());
+            Console.WriteLine(Result.ToString());
             Console.WriteLine("Hello, World!");
         }
     }

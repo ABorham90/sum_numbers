3029eeb54d0f23a347e0053fc787ae5b51f734e8
Ahmad Borham <ahmad.borham.dev@gmail.com>
2024-09-12 22:46:48 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace sum_numbers
+{
+    internal class Program
+    {
+        public static int sum(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+        static void Main(string[] args)
+        {
+            int result=sum(10, 20);
+            Console.WriteLine(result.ToString());
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
